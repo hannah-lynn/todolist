18a22b2a6fbf0df06f7fb7ebea4e73e66cd17b19
Hannahvic <hannahvl1987@hotmail.com>
2020-06-15 15:03:21 +0100

    adding new styling

--- todo.js
+++ todo.js
@@ -1,64 +1,54 @@
-var button = document.getElementById("enter");
-var input = document.getElementById("user input");
-var ul = document.getElementById("ulist");
-var list = document.querySelectorAll("li");
-
-
+var button = document.getElementById('enter');
+var input = document.getElementById('user-input');
+var ul = document.getElementById('ulist');
+var list = document.querySelectorAll('li');
 
 function inputLength() {
-	return input.value.length;
+  return input.value.length;
 }
 
 function createTodo() {
-	var li = document.createElement("li");
-	li.appendChild(document.createTextNode(input.value + ""));
-	li.className = "listItem";
-	var deleteBtn = document.createElement("button");
-	deleteBtn.className = "delete";
-	deleteBtn.appendChild(document.createTextNode("Delete"));
-	li.appendChild(deleteBtn);
-	ul.appendChild(li);
-	input.value = "";
-	li.addEventListener("click", toggleTodoAfterClick);
-	
+  var li = document.createElement('li');
+  li.appendChild(document.createTextNode(input.value + ' '));
+  li.className = 'listItem';
+  var deleteBtn = document.createElement('button');
+  deleteBtn.className = 'delete';
+  deleteBtn.appendChild(document.createTextNode('x'));
+  li.appendChild(deleteBtn);
+  ul.appendChild(li);
+  input.value = '';
+  li.addEventListener('click', toggleTodoAfterClick);
 }
 
 function addTodoAfterClick() {
-	if (inputLength() > 0) {   
-		createTodo()
-	}
+  if (inputLength() > 0) {
+    createTodo();
+  }
 }
 
-function addTodoAfterKeypress (event) {
-	if (inputLength() > 0 && event.which === 13) {
-		createTodo()
-	}
+function addTodoAfterKeypress(event) {
+  if (inputLength() > 0 && event.which === 13) {
+    createTodo();
+  }
 }
-
 
 function toggleTodoAfterClick() {
-        this.classList.toggle("done");
-    }
-
-
-
-function delOnClick(e) {
-	if(e.target.classList.contains("delete")){
-		var li = e.target.parentElement;
-			ul.removeChild(li);
-	}
+  this.classList.toggle('done');
 }
 
-list.forEach(function(li){
-	li.addEventListener("click", toggleTodoAfterClick);
+function delOnClick(e) {
+  if (e.target.classList.contains('delete')) {
+    var li = e.target.parentElement;
+    ul.removeChild(li);
+  }
+}
+
+list.forEach(function (li) {
+  li.addEventListener('click', toggleTodoAfterClick);
 });
 
-ul.addEventListener("click", delOnClick);
+ul.addEventListener('click', delOnClick);
 
-button.addEventListener("click", addTodoAfterClick);
+button.addEventListener('click', addTodoAfterClick);
 
-input.addEventListener("keypress", addTodoAfterKeypress);
-
-
-
-
+input.addEventListener('keypress', addTodoAfterKeypress);
